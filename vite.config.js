652aee639b36090de7f81e92320c3bcb5b6df4e3
PcombesVT <pierrecombes@hotmail.com>
2026-05-20 23:58:15 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      include: ['util'],
-      globals: { global: true }
-    })
-  ]
+  plugins: [react()],
+  define: {
+    'process.env': {},
+    'global': 'window'
+  }
 })
